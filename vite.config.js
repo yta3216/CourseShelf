946c196cbda4c5ad6e2f7b5432009ca0d65ba2d9
yta3216 <yta3216@gmail.com>
2026-04-19 00:24:41 -0700

    added script to run all tests sequentially

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [reactRouter(), tailwindcss()],
+  test: {
+    exclude: ["app/tests/e2e/**", "node_modules/**"]
+  }
 });
